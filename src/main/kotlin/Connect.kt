7ba8287108da5f77e1d50211301e2ed0b8942e44
Enrique M. Talavera <kiquetal@pop-os.localdomain>
2021-06-05 21:24:49 -0400

    easy kotlin pgsql with logger

---
+++ src/main/kotlin/Connect.kt
@@ -0,0 +1,36 @@
+
+import com.github.jasync.sql.db.RowData
+import com.github.jasync.sql.db.pool.ConnectionPool
+import com.github.jasync.sql.db.postgresql.PostgreSQLConnection
+import com.github.jasync.sql.db.postgresql.PostgreSQLConnectionBuilder
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import java.text.MessageFormat
+import java.util.function.Consumer
+fun createClient(): ConnectionPool<PostgreSQLConnection> {
+    return PostgreSQLConnectionBuilder.createConnectionPool(
+        "jdbc:postgresql://localhost:5432/music?user=pg4e&password=paraguay"
+    )
+}
+
+
+class Connect {
+
+   companion object {
+        @JvmStatic
+       fun main(args: Array<String>) {
+            var logger: Logger = LogManager.getLogger()
+
+            val postgres = createClient()
+            val conn =   postgres.connect()
+            val result=    conn.get().sendPreparedStatement("select * from artist");
+            val rows = result.get().rows;
+            rows.stream().forEach(Consumer { t:RowData ->
+                logger.info(MessageFormat.format("Id={0}|Name={1}\n",t.get(0),t.get(1)))
+
+            })
+       }
+
+   }
+
+}
